e3ab9c28597a0be9ba5ee8b8a24a25968a16c211
John Hamm <hammjm125@yahoo.com>
2017-08-21 12:05:57 -0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+/* Because everyone needs to start with a Hello World! */
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello World");
+   }
+}
