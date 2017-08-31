d3dedcf37eaa5cbecd42fb0836af6ae97d829269
John Hamm <hammjm125@yahoo.com>
2017-08-31 13:16:41 -0400

    Create factorials.java

---
+++ factorials.java
@@ -0,0 +1,17 @@
+/* Initially did this in R, so transferred it to Java to help learn the language */
+
+public class Factorial
+{
+	public static void main(String[] args)
+	{	final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
+}
